174190c45e8ee9f2726c034643b6e0f1313b668d
vijayaprm <vijayapratapreddy.m@gmail.com>
2023-05-18 16:03:58 +0530

    added intial  css

--- src/Form.js
+++ src/Form.js
@@ -1,26 +1,29 @@
+import './Form.css'
 function Form(){
     
     return(
-        <div>
-            <label>Generate content for single listing</label>
+        <div className='Form'>
+            <label className='title'>Generate content for single listing</label>
             <div class="">
-                <form name="myForm">
-                    <div>
+                <form>
+                    <div className=''>
                         <label>Product</label>
                         <input type="text" name="Product" id ="Product" placeholder="Brand,Product_Name" required />
                         
                     </div>
+                    <div className='container'>
                     <div>
                         <label>Keywords</label>
-                        <textarea rows="6" type="text" name="Keywords" id="Keywords" ></textarea>
+                        <textarea rows="9" type="text" name="Keywords" id="Keywords" ></textarea>
                     </div>
                     <div>
                         <label>Features</label>
-                        <textarea rows="6" type="text" name="Features" id="Features" ></textarea>   
+                        <textarea rows="9" type="text" name="Features" id="Features" ></textarea>   
                     </div>
                     <div>
                         <label>Search</label>
-                        <textarea rows="6" type="text" name="search" id="search"></textarea>
+                        <textarea rows="9" type="text" name="search" id="search"></textarea>
+                    </div>
                     </div>
                     <div>
                         <button value="submit" id="submit" type="submit" >Gerate Catalog</button>
