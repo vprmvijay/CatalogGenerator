7e7b816f6debe50bf81566e91f3a31b245e04c7d
vijayaprm <vijayapratapreddy.m@gmail.com>
2023-05-18 16:41:46 +0530

    minor changes

--- src/Form.js
+++ src/Form.js
@@ -3,29 +3,30 @@ function Form(){
     
     return(
         <div className='Form'>
+            <label className='Head'>Amazon Catalog Generator</label>
             <label className='title'>Generate content for single listing</label>
             <div class="">
                 <form>
                     <div className=''>
                         <label>Product</label>
-                        <input type="text" name="Product" id ="Product" placeholder="Brand,Product_Name" required />
+                        <input type="text" name="Product" id ="Product" placeholder="Brand_Name,Product_Name" required />
                         
                     </div>
                     <div className='container'>
                     <div>
                         <label>Keywords</label>
-                        <textarea rows="9" type="text" name="Keywords" id="Keywords" ></textarea>
+                        <textarea rows="9" type="text" name="Keywords" id="Keywords" placeholder="Values must be seperated by comma(,)" required></textarea>
                     </div>
                     <div>
                         <label>Features</label>
-                        <textarea rows="9" type="text" name="Features" id="Features" ></textarea>   
+                        <textarea rows="9" type="text" name="Features" id="Features" placeholder="Values must be seperated by comma(,)" required></textarea>   
                     </div>
                     <div>
-                        <label>Search</label>
-                        <textarea rows="9" type="text" name="search" id="search"></textarea>
+                        <label>Search Terms</label>
+                        <textarea rows="9" type="text" name="search" id="search" placeholder="Values must be seperated by comma(,)"></textarea>
                     </div>
                     </div>
-                    <div>
+                    <div className='but'>
                         <button value="submit" id="submit" type="submit" >Gerate Catalog</button>
                     </div>  
                 </form>
