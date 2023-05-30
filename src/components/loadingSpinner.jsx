e96e7beea1e873d3d3050d6fde20ebac16f99ec4
vijayaprm <vijayapratapreddy.m@gmail.com>
2023-05-30 16:20:49 +0530

    integrated with backend-v1

---
+++ src/components/loadingSpinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LoadingSpinner.css'; // Import the CSS file for styling the loading spinner
+
+const LoadingSpinner = () => {
+  return (
+    <div className="loading-spinner-container">
+      <div className="loading-spinner"></div>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
